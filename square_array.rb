e23aaa90df5cdfb08c0b3ccbe977e9b186fcb0d5
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-17 00:23:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,9 @@ def square_array(array)
   squared = [1,2,3]
   array.each do |square|
     squared << square**2
-    square**2 << square**2 
   end
   return squared
+  squared << square**2 
+end
+return squared
 end

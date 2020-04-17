13c65455464991744411f786f2c220310660f448
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-17 00:39:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,8 +6,8 @@ def square_array(array)
   return squared
 end
 
-squared = [1,2,3]
-  array.each do |square|
-    squared << square**2
-  end
-  return squared
+def square_array(array)
+  new_array = []
+  array.each {|number| new_array << number **2}
+  new_array
+end
